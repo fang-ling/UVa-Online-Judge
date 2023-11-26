7ec0a6c8218f403f872207296182f54fe2cf1ece
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-11-26 23:00:46 +0800

    Add notes for 272

--- Solution Set Volumes/Volume 2 (200-299)/272.c
+++ Solution Set Volumes/Volume 2 (200-299)/272.c
@@ -5,6 +5,14 @@
 //  Created by Fang Ling on 2023/11/25.
 //
 
+/*
+ * This program uses a flag `is_left` to keep track of whether we are inside an
+ * open quote or not. When we encounter a double-quote character, we check the
+ * flag to determine whether it should be replaced by "``" or "''". We toggle
+ * the flag after each replacement to alternate between opening and closing
+ * quotes.
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 
