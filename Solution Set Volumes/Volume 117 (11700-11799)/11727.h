36296a986b26e3d0762bd871eb4dbb4c038edf1f
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-12-08 16:04:38 +0800

    XXIII 11727 ✔️

---
+++ Solution Set Volumes/Volume 117 (11700-11799)/11727.h
@@ -0,0 +1,13 @@
+//
+//  11727.h
+//  hxy
+//
+//  Created by Fang Ling on 2023/12/8.
+//
+
+#ifndef _11727_h
+#define _11727_h
+
+void main_11727(void);
+
+#endif /* _11727_h */
--- hxy_tests/hxy_tests.m
+++ hxy_tests/hxy_tests.m
@@ -39,6 +39,8 @@
 
 #import "11292.h"
 
+#import "11727.h"
+
 #import "12720.h"
 
 #import <XCTest/XCTest.h>
@@ -769,6 +771,43 @@
       stringWithContentsOfFile: [
         path stringByAppendingString:
           [NSString stringWithFormat: @"11292.%d.out", i]
+      ]
+      encoding: NSUTF8StringEncoding
+      error: nil
+    ];
+    var sol = [
+      NSString
+      stringWithContentsOfFile: @"/tmp/hxy.out"
+      encoding: NSUTF8StringEncoding
+      error: nil
+    ];
+    XCTAssertEqualObjects(sol, out);
+  }
+}
+
+// MARK: - Volume 117
+
+- (void)test_11727 {
+  NSString* path = @IO_DATA_PATH;
+  path = [path stringByAppendingString: @"Volume 117 (11700-11799)/"];
+  for (var i = 0; i < 5; i += 1) {
+    freopen(
+      [[path
+        stringByAppendingString: [NSString stringWithFormat: @"11727.%d.in", i]
+       ] UTF8String
+      ],
+      "r",
+      stdin
+    );
+    freopen("/tmp/hxy.out", "w", stdout);
+    main_11727();
+    fclose(stdin);
+    fclose(stdout);
+    var out = [
+      NSString
+      stringWithContentsOfFile: [
+        path stringByAppendingString:
+          [NSString stringWithFormat: @"11727.%d.out", i]
       ]
       encoding: NSUTF8StringEncoding
       error: nil
