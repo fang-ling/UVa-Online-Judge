cd6d885cf055a5a48c5a7b2f9eb42839efd7a80b
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2024-02-14 14:37:48 +0800

    XLVI 815 ✔️

---
+++ Solution Set Volumes/Volume 8 (800-899)/815.c
@@ -0,0 +1,447 @@
+//
+//  815.c
+//  hxy
+//
+//  Created by Fang Ling on 2024/2/14.
+//
+
+#include <stdio.h>
+
+/*===----------------------------------------------------------------------===*/
+/*                                                        ___   ___           */
+/* Array START                                          /'___\ /\_ \          */
+/*                                                     /\ \__/ \//\ \         */
+/* Author: Fang Ling (fangling@fangl.ing)              \ \ ,__\  \ \ \        */
+/* Version: 1.6                                         \ \ \_/__ \_\ \_  __  */
+/* Date: January 4, 2024                                 \ \_\/\_\/\____\/\_\ */
+/*                                                        \/_/\/_/\/____/\/_/ */
+/*===----------------------------------------------------------------------===*/
+
+/*
+ * This source file is part of the C Collections open source project
+ *
+ * Copyright (c) 2023 Fang Ling
+ * Licensed under Apache License v2.0
+ *
+ * See https://github.com/fang-ling/C-Collections/blob/main/LICENSE for license
+ * information
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*===----------------------------------------------------------------------===*/
+/*                                                        ___   ___           */
+/* sort START                                           /'___\ /\_ \          */
+/*                                                     /\ \__/ \//\ \         */
+/* Author: Fang Ling (fangling@fangl.ing)              \ \ ,__\  \ \ \        */
+/* Version: 1.0                                         \ \ \_/__ \_\ \_  __  */
+/* Date: December 4, 2023                                \ \_\/\_\/\____\/\_\ */
+/*                                                        \/_/\/_/\/____/\/_/ */
+/*===----------------------------------------------------------------------===*/
+
+/*
+ * This source file is part of the C Collections open source project
+ *
+ * Copyright (c) 2023 Fang Ling
+ * Licensed under Apache License v2.0
+ *
+ * See https://github.com/fang-ling/C-Collections/blob/main/LICENSE for license
+ * information
+ */
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define var __auto_type
+
+#define INS_THR 64
+
+/* Byte-wise swap two items of size SIZE. */
+#define SWAP(a, b, size)      \
+  do {                        \
+    unsigned __size = (size); \
+    char *__a = (a);          \
+    char *__b = (b);          \
+    do {                      \
+      char __tmp = *__a;      \
+      *__a++ = *__b;          \
+      *__b++ = __tmp;         \
+    } while (--__size > 0);   \
+  } while (0);
+
+static int partition(
+  void* base,
+  int width,
+  int p,
+  int r,
+  int (*compare)(const void*, const void*)
+) {
+  var x_ptr = base + width * r;
+  var i = p - 1;
+  var j = 0;
+  for (j = p; j < r; j += 1) {
+    if (compare(base + width * j, x_ptr) <= 0) {
+      i += 1;
+      SWAP(base + width * i, base + width * j, width);
+    }
+  }
+  SWAP(base + width * (i + 1), base + width * r, width);
+  return i + 1;
+}
+
+static int randomized_partition(
+  void* base,
+  int width,
+  int p,
+  int r,
+  int (*compare)(const void*, const void*)
+) {
+  var i = p + random() % (r - p + 1);
+  SWAP(base + width * r, base + width * i, width);
+  return partition(base, width, p, r, compare);
+}
+
+static void _wkq_insertion_sort(
+  void* base,
+  int width,
+  int p,
+  int r,
+  int (*compare)(const void*, const void*)
+) {
+  var j = p + 1;
+  for (; j <= r; j += 1) {
+    var i = j;
+    while (i > 0 && compare(base + width * (i - 1), base + width * i) > 0) {
+      SWAP(base + width * (i - 1), base + width * i, width);
+      i -= 1;
+    }
+  }
+}
+
+static void _wkq_quicksort(
+  void* base,
+  int width,
+  int p,
+  int r,
+  int (*compare)(const void*, const void*)
+) {
+  if (p < r) {
+    var is_sorted = true;
+    var i = 0;
+    for (i = p + 1; i <= r; i += 1) {
+      if (compare(base + width * i, base + width * (i - 1)) < 0) {
+        is_sorted = false;
+        break;
+      }
+    }
+
+    var is_reverse_sorted = true;
+    for (i = r; i > p; i -= 1) {
+      if (compare(base + width * i, base + width * (i - 1)) > 0) {
+        is_reverse_sorted = false;
+      }
+    }
+    if (is_reverse_sorted) {
+      i = p;
+      var j = r;
+      while (j > i) {
+        SWAP(base + width * i, base + width * j, width);
+        i += 1;
+        j -= 1;
+      }
+    } else if (!is_sorted) {
+      if (r - p + 1 <= INS_THR) {
+        _wkq_insertion_sort(base, width, p, r, compare);
+      } else {
+        var q = randomized_partition(base, width, p, r, compare);
+        _wkq_quicksort(base, width, p, q - 1, compare);
+        _wkq_quicksort(base, width, q + 1, r, compare);
+      }
+    }
+  }
+}
+
+static void sort(
+  void* base,
+  int nel,
+  int width,
+  int (*compare)(const void*, const void*)
+) {
+  srandom(1935819342);
+  _wkq_quicksort(base, width, 0, nel - 1, compare);
+}
+
+/*===----------------------------------------------------------------------===*/
+/*             ___                            ___                             */
+/*           /'___\                          /\_ \    __                      */
+/*          /\ \__/   __      ___      __    \//\ \  /\_\    ___      __      */
+/*          \ \ ,__\/'__`\  /' _ `\  /'_ `\    \ \ \ \/\ \ /' _ `\  /'_ `\    */
+/*           \ \ \_/\ \L\.\_/\ \/\ \/\ \L\ \    \_\ \_\ \ \/\ \/\ \/\ \L\ \   */
+/*            \ \_\\ \__/.\_\ \_\ \_\ \____ \   /\____\\ \_\ \_\ \_\ \____ \  */
+/*             \/_/ \/__/\/_/\/_/\/_/\/___L\ \  \/____/ \/_/\/_/\/_/\/___L\ \ */
+/* sort END                            /\____/                        /\____/ */
+/*                                     \_/__/                         \_/__/  */
+/*===----------------------------------------------------------------------===*/
+
+
+#define var __auto_type
+
+/*
+ * Error code of Array:
+ * 0: NO ERROR
+ * 1: due to malloc, check `errno`
+ * 2: Array index is out of range
+ * 3: Negative Array index is out of range
+ * 5: due to realloc, check `errno`
+ * 6: Can't remove last element from an empty collection
+ */
+
+struct Array {
+  void* _storage;
+  
+  /* The number of elements in the array. */
+  int count;
+  
+  /*
+   * The total number of elements that the array can contain without
+   * allocating new storage.
+   *
+   * Every array reserves a specific amount of memory to hold its contents.
+   * When you add elements to an array and that array begins to exceed its
+   * reserved capacity, the array allocates a larger region of memory and
+   * copies its elements into the new storage. The new storage is a multiple
+   * of the old storage's size. This exponential growth strategy means that
+   * appending an element happens in constant time, averaging the performance
+   * of many append operations. Append operations that trigger reallocation
+   * have a performance cost, but they occur less and less often as the array
+   * grows larger.
+   */
+  int capacity;
+  
+  /* The size of stored Element type. */
+  int element_size;
+  
+  /* A Boolean value indicating whether or not the array is empty. */
+  bool is_empty;
+};
+
+/* MARK: - Creating and Destroying an Array */
+
+static
+int _array_init(struct Array* array, unsigned int count, int element_size) {
+  /* Rounding up to next power of 2 */
+  var capacity = count - 1;
+  capacity |= capacity >> 1;
+  capacity |= capacity >> 2;
+  capacity |= capacity >> 4;
+  capacity |= capacity >> 8;
+  capacity |= capacity >> 16;
+  capacity += 1;
+  
+  if (count == 0) {
+    (*array)._storage = NULL;
+  } else {
+    (*array)._storage = malloc(capacity * element_size);
+    if ((*array)._storage == NULL) {
+      return 1;
+    }
+  }
+  (*array).element_size = element_size;
+  (*array).capacity = capacity;
+  (*array).count = count;
+  (*array).is_empty = count == 0 ? true : false;
+  
+  return 0;
+}
+
+/* Creates an empty array. */
+static int array_init(struct Array* array, int element_size) {
+  return _array_init(array, 0, element_size);
+}
+
+/* Destroys an array. */
+static void array_deinit(struct Array* array) {
+  free((*array)._storage);
+  (*array).count = 0;
+  (*array).element_size = 0;
+  (*array).capacity = 0;
+  (*array).is_empty = true;
+}
+
+/* MARK: - Accessing Elements */
+
+/* Check that the specified `index` is valid, i.e. `0 ≤ index < count`. */
+static int _check_index(struct Array* array, int index) {
+  if (index >= (*array).count) {
+    return 2;
+  } else if (index < 0) {
+    return 3;
+  }
+  return 0;
+}
+
+/* Returns the element at the specified position. */
+static int array_get(struct Array* array, int index, void* element) {
+  var err = _check_index(array, index);
+  if (err != 0) {
+    return err;
+  }
+  memcpy(
+    element,
+    (*array)._storage + (*array).element_size * index,
+    (*array).element_size
+  );
+  return err;
+}
+
+/* Replaces the element at the specified position. */
+static int array_set(struct Array* array, int index, void* element) {
+  var err = _check_index(array, index);
+  if (err != 0) {
+    return err;
+  }
+  memcpy(
+    (*array)._storage + (*array).element_size * index,
+    element,
+    (*array).element_size
+  );
+  return err;
+}
+
+/* MARK: - Adding Elements */
+
+/* Adds a new element at the end of the array. */
+static int array_append(struct Array* array, void* element) {
+  var q = 2; /* multiple factor = 2 */
+  
+  if ((*array).capacity == 0) {
+    (*array)._storage = realloc((*array)._storage, 1 * (*array).element_size);
+    (*array).capacity = 1;
+  }
+  if ((*array).count == (*array).capacity) {
+    (*array).capacity *= q;
+    var new_size = (*array).capacity * (*array).element_size;
+    (*array)._storage = realloc((*array)._storage, new_size);
+    if ((*array)._storage == NULL) {
+      /* FIXME: memory leak will happen if realloc failed */
+      return 5;
+    }
+  }
+  (*array).count += 1;
+  (*array).is_empty = false;
+  array_set(array, (*array).count - 1, element);
+  
+  return 0;
+}
+
+/* MARK: - Removing Elements */
+
+/* Removes all the elements. */
+static int array_remove_all(struct Array* array) {
+  free((*array)._storage);
+  (*array)._storage = NULL;
+  (*array).count = 0;
+  (*array).capacity = 0;
+  (*array).is_empty = true;
+  
+  return 0;
+}
+
+/* MARK: - Reordering an Array’s Elements */
+
+/* Sorts the collection in place. */
+static
+void array_sort(struct Array* array, int (*compare)(const void*, const void*)) {
+  if ((*array).count <= 1) {
+    return;
+  }
+  sort((*array)._storage, (*array).count, (*array).element_size, compare);
+}
+
+/*===----------------------------------------------------------------------===*/
+/*             ___                            ___                             */
+/*           /'___\                          /\_ \    __                      */
+/*          /\ \__/   __      ___      __    \//\ \  /\_\    ___      __      */
+/*          \ \ ,__\/'__`\  /' _ `\  /'_ `\    \ \ \ \/\ \ /' _ `\  /'_ `\    */
+/*           \ \ \_/\ \L\.\_/\ \/\ \/\ \L\ \    \_\ \_\ \ \/\ \/\ \/\ \L\ \   */
+/*            \ \_\\ \__/.\_\ \_\ \_\ \____ \   /\____\\ \_\ \_\ \_\ \____ \  */
+/*             \/_/ \/__/\/_/\/_/\/_/\/___L\ \  \/____/ \/_/\/_/\/_/\/___L\ \ */
+/* Array END                           /\____/                        /\____/ */
+/*                                     \_/__/                         \_/__/  */
+/*===----------------------------------------------------------------------===*/
+
+static int cmp_815(const void* a, const void* b) {
+  if (*(int*)a < *(int*)b) {
+    return -1;
+  } else if (*(int*)a > *(int*)b) {
+    return 1;
+  }
+  return 0;
+}
+
+void main_815(void) {
+  struct Array regions;
+  array_init(&regions, sizeof(int));
+  
+  var region_count = 0;
+  
+  var n = 0;
+  var m = 0;
+  while (scanf("%d %d", &n, &m) == 2 && n != 0 && m != 0) {
+    array_remove_all(&regions);
+    
+    var i = 0;
+    var len = n * m;
+    for (i = 0; i < len; i += 1) {
+      var delta = 0;
+      scanf("%d", &delta);
+      array_append(&regions, &delta);
+    }
+    
+    var water = 0;
+    scanf("%d", &water);
+    array_sort(&regions, cmp_815);
+    var water_count = 0;
+    var water_level = 0.0;
+    /*array_get(&regions, 0, &water_level); //dont have same memory layout */
+    water_level = ((int*)regions._storage)[0];
+    for (i = 0; i < regions.count; i += 1) {
+      if (water > 0) {
+        if (i != regions.count - 1) { /* not the highest region */
+          var _a = 0;
+          var _b = 0;
+          array_get(&regions, i + 1, &_a);
+          array_get(&regions, i, &_b);
+          var delta = _a - _b;
+          var alpha = 10 * (i + 1) * 10 * delta;
+          if (water < alpha) {
+            water_level += (double)water / 10.0 / (10.0 * (i + 1));
+            water -= water;
+          } else {
+            water_level += delta;
+            water -= alpha;
+          }
+        } else { /* hightest region with water */
+          water_level += (double)water / 10.0 / (10.0 * (i + 1));
+          water -= water;
+        }
+      }
+    }
+    var unsafe_cast = (int*)regions._storage;
+    for (i = 0; i < regions.count; i += 1) {
+      if (water_level > unsafe_cast[i]) {
+        water_count += 1;
+      }
+    }
+    printf("Region %d\n", region_count += 1);
+    printf("Water level is %.2lf meters.\n", water_level);
+    printf(
+      "%.2lf percent of the region is under water.\n\n",
+      (double)water_count / regions.count * 100
+    );
+  }
+  
+  array_deinit(&regions);
+}
---
+++ Solution Set Volumes/Volume 8 (800-899)/815.h
@@ -0,0 +1,13 @@
+//
+//  815.h
+//  hxy
+//
+//  Created by Fang Ling on 2024/2/14.
+//
+
+#ifndef _815_h
+#define _815_h
+
+void main_815(void);
+
+#endif /* _815_h */
