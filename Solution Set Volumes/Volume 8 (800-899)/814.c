bd5aa03f86b66a0ed9853f874a83924e7cb87fb7
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2024-04-14 17:50:35 +0800

    LXXII 814 ✔️

--- Solution Set Volumes/Volume 8 (800-899)/814.c
+++ Solution Set Volumes/Volume 8 (800-899)/814.c
@@ -217,97 +217,6 @@ static bool array_contains(
 /*                                     \_/__/                         \_/__/  */
 /*===----------------------------------------------------------------------===*/
 
-/* The strsep() function first appeared in 4.4BSD. */
-/*
- * Copyright (c) 1999 Apple Computer, Inc. All rights reserved.
- *
- * @APPLE_LICENSE_HEADER_START@
- *
- * The contents of this file constitute Original Code as defined in and
- * are subject to the Apple Public Source License Version 1.1 (the
- * "License").  You may not use this file except in compliance with the
- * License.  Please obtain a copy of the License at
- * http://www.apple.com/publicsource and read it before using this file.
- *
- * This Original Code and all software distributed under the License are
- * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
- * License for the specific language governing rights and limitations
- * under the License.
- *
- * @APPLE_LICENSE_HEADER_END@
- */
-/*
- * Copyright (c) 1990, 1993
- *  The Regents of the University of California.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *  This product includes software developed by the University of
- *  California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-/*
- * Get next token from string *stringp, where tokens are possibly-empty
- * strings separated by characters from delim.
- *
- * Writes NULs into the string at *stringp to end tokens.
- * delim need not remain constant from call to call.
- * On return, *stringp points past the last NUL written (if there might
- * be further tokens), or is NULL (if there are definitely no more tokens).
- *
- * If *stringp is NULL, strsep returns NULL.
- */
-static char* hxy_strsep(char** stringp, const char* delim) {
-  char *s;
-  const char *spanp;
-  int c, sc;
-  char *tok;
-  
-  if ((s = *stringp) == NULL)
-    return (NULL);
-  for (tok = s;;) {
-    c = *s++;
-    spanp = delim;
-    do {
-      if ((sc = *spanp++) == c) {
-        if (c == 0)
-          s = NULL;
-        else
-          s[-1] = 0;
-        *stringp = s;
-        return (tok);
-      }
-    } while (sc != 0);
-  }
-  /* NOTREACHED */
-}
-
 #define LEN 2048
 
 struct MTA {
@@ -338,7 +247,6 @@ void main_814(void) {
   struct MTA mta;
   struct User u;
   struct User sender;
-  char* token = NULL;
   var buf = (char*)malloc(sizeof(char) * LEN);
   
   /* Read MTAs */
@@ -365,29 +273,69 @@ void main_814(void) {
     rcpts_cnt = 0;
     
     /* Read RCPTs */
-    var tofree = strdup(buf);
+    /*var tofree = strdup(buf);
     var is_first = true;
     while ((token = hxy_strsep(&tofree, "@")) != NULL) {
       if (is_first) {
-        is_first = false;
-        strcpy(sender.name, token);
-      } else {
-        strcpy(sender.MTA, token);
+        is_first = false;*/
+    var at_i = 0;
+    var at_count = (int)strlen(buf);
+    for (at_i = 0; at_i < at_count; at_i += 1) {
+      if (buf[at_i] == '@') {
+        break;
       }
     }
-    free(tofree);
+    memset(sender.name, 0, LEN);
+    memset(sender.MTA, 0, LEN);
+    var ii = 0;
+    for (ii = 0; ii < at_i; ii += 1) {
+      sender.name[ii] = buf[ii];
+    }
+    sender.name[ii] = '\0';
+    /*strcpy(sender.name, "test");*/
+      /*} else {*/
+    
+    for (ii = at_i + 1; ii < at_count; ii += 1) {
+      sender.MTA[ii - at_i - 1] = buf[ii];
+    }
+    sender.MTA[ii] = '\0';
+        /*strcpy(sender.MTA, "test");*/
+      /*}
+    }
+    free(tofree);*/
     while (scanf("%s", buf) == 1 && buf[0] != '*') {
-      tofree = strdup(buf);
+      
+      at_i = 0;
+      at_count = (int)strlen(buf);
+      for (at_i = 0; at_i < at_count; at_i += 1) {
+        if (buf[at_i] == '@') {
+          break;
+        }
+      }
+      memset(u.name, 0, LEN);
+      memset(u.MTA, 0, LEN);
+      var ii = 0;
+      for (ii = 0; ii < at_i; ii += 1) {
+        u.name[ii] = buf[ii];
+      }
+      u.name[ii] = '\0';
+      /*strcpy(sender.name, "test");*/
+        /*} else {*/
+      for (ii = at_i + 1; ii < at_count; ii += 1) {
+        u.MTA[ii - at_i - 1] = buf[ii];
+      }
+      u.MTA[ii] = '\0';
+      /*tofree = strdup(buf);
       is_first = true;
       while ((token = hxy_strsep(&tofree, "@")) != NULL) {
         if (is_first) {
-          is_first = false;
-          strcpy(u.name, token);
-        } else {
-          strcpy(u.MTA, token);
-        }
+          is_first = false;*/
+          
+        /*} else {*/
+          
+        /*}
       }
-      free(tofree);
+      free(tofree);*/
       
       var contains = false;
       var j = 0;
